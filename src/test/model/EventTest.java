86a0b3e1db4e0d720b503639cb97b62b22709021
louisluo <lluo11@student.cs.ubc.ca>
2023-04-02 02:28:09 -0700

    added more tests to fill coverage

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -6,27 +6,44 @@ import org.junit.jupiter.api.Test;
 import java.util.Calendar;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 //Test class for Events, cited from AlarmSystem.
 public class EventTest {
     private Event e;
+    private Event same;
+    private Event different;
+    private Event empty;
     private Date d;
 
     @BeforeEach
     public void runBefore() {
-        e = new Event("Sensor open at door");   // (1)
-        d = Calendar.getInstance().getTime();   // (2)
+        e = new Event("thing");
+        d = Calendar.getInstance().getTime();
+        same = new Event("same");
+        different = new Event("different");
     }
 
     @Test
     public void testEvent() {
-        assertEquals("Sensor open at door", e.getDescription());
+        assertEquals("thing", e.getDescription());
         assertEquals(d.toString(), e.getDate().toString());
     }
 
     @Test
+    void hashCodeTest() {
+        int num = e.hashCode();
+        assertEquals(e.hashCode(), num);
+    }
+
+    @Test
+    void EqualsTest() {
+        assertFalse(same.equals(different));
+        assertFalse(same.equals(empty));
+    }
+
+    @Test
     public void testToString() {
-        assertEquals(d.toString() + "\n" + "Sensor open at door", e.toString());
+        assertEquals(d.toString() + "\n" + "thing", e.toString());
     }
 }
